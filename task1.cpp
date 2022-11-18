764c37021dced347489400933fcb8f751c7bd6bd
Roman Golik <romangolik86786@gmail.com>
2022-11-18 09:47:13 +0200

    Renamed variable

--- task1.cpp
+++ task1.cpp
@@ -16,17 +16,17 @@ int main() {
     for (int i = 0; i < size; i++)
         mas[i] = i;
 
-    int NumThread = 10;
-    int beginMas[NumThread];
-    int endMas[NumThread];
-    for (int i = 0; i < NumThread; i++)
-        beginMas[i] = size / NumThread * i;
-    for (int i = 0; i < NumThread - 1; i++)
+    int NumParts = 10;
+    int beginMas[NumParts];
+    int endMas[NumParts];
+    for (int i = 0; i < NumParts; i++)
+        beginMas[i] = size / NumParts * i;
+    for (int i = 0; i < NumParts - 1; i++)
         endMas[i] = beginMas[i + 1] - 1;
 
-    endMas[NumThread - 1] = size - 1;
+    endMas[NumParts - 1] = size - 1;
 
-    for (int i = 0; i < NumThread; i++) {
+    for (int i = 0; i < NumParts; i++) {
         SumMas += findPartSum(mas, beginMas[i], endMas[i]);
     }
 
